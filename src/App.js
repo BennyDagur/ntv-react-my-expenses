e50fcd35f2893e05916f881b1c2204ce3bc50c97
BennyDagur <bdg2@nemandi.ntv.is>
2022-11-13 17:48:57 +0000

    Complete state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import { useState, useRef } from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+	const [displayValue, setDisplayValue] = useState([]);
+  
+  const remove = useRef([0])
+  console.log(remove);
+
+	const inputItem = useRef("");
+  const inputValue = useRef(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h2 className="Main-text">Current Items: [{displayValue.toString()}]</h2>
+          <h2 className="Main-text">{count}</h2>
+
+          <label className="Label-values">Items</label>
+          <input className="Input-values" onChange={(itemEvent) => {
+            inputItem.current = itemEvent.target.value
+          }}/>
+
+          <label className="Label-values">Values</label>
+          <input className="Input-values" onChange={(countEvent) => {
+            inputValue.current = countEvent.target.value
+          }}/>
+
+          <button className="Submit-button" type="button" onClick={() => {
+            if(inputValue.current != 0 && inputItem.current.length > 0){
+            setDisplayValue(displayValue.concat(inputItem.current))
+            setCount(parseInt(inputValue.current) + count)
+            remove.current.push(inputValue.current)
+          } else {}
+          }}>Add Items</button>
+          
+          <button className="Submit-button" type="button" onClick={() => {
+            if(count != 0){
+            setDisplayValue(displayValue.slice(0,-1))
+            setCount(count - remove.current.pop())
+          } else {}
+          }}>Remove Items</button>
       </header>
     </div>
   );
